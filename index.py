1ced17d1203a1307ec49462e886f4928c6b9ec03
AdithyaHS <43458852+AdithyaHS@users.noreply.github.com>
2019-04-17 23:06:30 -0400

    Update index.py

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ CORS(app)
 
 #Configurations for MongoDB
 projectsTableURI = 'mongodb://DHANUMAN:Target2018atiub@ds113855.mlab.com:13855/project_add'
-UsermanagementURI= 'mongodb://hsadi:adash569@cluster0-shard-00-00-5zv82.mongodb.net:27017,cluster0-shard-00-01-5zv82.mongodb.net:27017,cluster0-shard-00-02-5zv82.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+UsermanagementURI= 'mongodb+srv://hsadi:adash569@cluster0-5zv82.mongodb.net/test?retryWrites=true'
 #client = MongoClient(projectsTableURI)
 client1= MongoClient(UsermanagementURI)
 
