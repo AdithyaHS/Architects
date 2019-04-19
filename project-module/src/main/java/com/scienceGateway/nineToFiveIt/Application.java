ef8ef40048e8faf9947a00c75c2c499206c803f0
AdithyaHS <adithyahs1993@gmail.com>
2019-04-18 23:50:49 -0400

    removing zookeeper-- continued

--- project-module/src/main/java/com/scienceGateway/nineToFiveIt/Application.java
+++ project-module/src/main/java/com/scienceGateway/nineToFiveIt/Application.java
@@ -15,7 +15,7 @@ import java.net.URL;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws InterruptedException, KeeperException, IOException {
+	public static void main(String[] args){
 		SpringApplication.run(Application.class, args);
 		/*ZooKeeperConnection zooKeeperConnection=new ZooKeeperConnection();
 
