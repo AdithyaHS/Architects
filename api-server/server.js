8c8eac4205e9cba0dd4ef205bb1538ef65af61a3
AdithyaHS <adithyahs1993@gmail.com>
2019-04-23 13:25:29 -0400

    removing zookeeper

--- api-server/server.js
+++ api-server/server.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 const passport = require("passport");
 const register = require("./static/Registry.json");
-const ZooKeeper = require('node-zookeeper-client');
-const publicIp = require('public-ip');
+//const ZooKeeper = require('node-zookeeper-client');
+//const publicIp = require('public-ip');
 const app=express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -26,27 +26,27 @@ app.use(passport.initialize());
 
   app.listen(port, () => console.log(`Server started on port ${port}`));
 
-  const client = ZooKeeper.createClient('149.165.171.39:2181',{ sessionTimeout: 5000 });
+//   const client = ZooKeeper.createClient('149.165.171.39:2181',{ sessionTimeout: 5000 });
 
-  publicIp.v4().then(ip => {
-    console.log("your public ip address", ip);
-    client.once('connected', function () {
-    console.log('Connected to the server.');
+//   publicIp.v4().then(ip => {
+//     console.log("your public ip address", ip);
+//     client.once('connected', function () {
+//     console.log('Connected to the server.');
      
-        client.create("/ensemble/apiServer",new Buffer(ip +":"+port),ZooKeeper.CreateMode.EPHEMERAL, function (error) {
-            if (error) {
-                console.log('Failed to create node: %s due to: %s.', error);
-            } else {
-                console.log('Node: %s is successfully created.');
-            }
+//         client.create("/ensemble/apiServer",new Buffer(ip +":"+port),ZooKeeper.CreateMode.EPHEMERAL, function (error) {
+//             if (error) {
+//                 console.log('Failed to create node: %s due to: %s.', error);
+//             } else {
+//                 console.log('Node: %s is successfully created.');
+//             }
      
-            //client.close();
-        });
-    });
-    client.connect();
-  });
+//             //client.close();
+//         });
+//     });
+//     client.connect();
+//   });
  // console.log("here")
  //console.log(client)
   
 
-module.exports.client=client; //this is a pure hack!!! should be removed sometime later
+//module.exports.client=client; //this is a pure hack!!! should be removed sometime later
